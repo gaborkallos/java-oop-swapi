45fcd09b2b479ba071c9a3a52c805d9e11397bbe
gaborkallos <gaborkallos@gmail.com>
2019-02-28 20:06:09 +0100

    headers for html with back to mainscreen button

---
+++ src/main/java/com/codecool/swapi/controller/PeopleController.java
@@ -0,0 +1,55 @@
+package com.codecool.swapi.controller;
+
+import com.codecool.swapi.config.TemplateEngineUtil;
+import com.codecool.swapi.models.People;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.client.api.ContentResponse;
+import org.eclipse.jetty.client.api.Request;
+import org.eclipse.jetty.http.HttpHeader;
+import org.eclipse.jetty.http.HttpMethod;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/character"})
+public class PeopleController extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
+        WebContext context = new WebContext(request, response, request.getServletContext());
+        response.setCharacterEncoding("utf-8");
+        String url = request.getParameter("character");
+        if (url != null) {
+            HttpClient httpClient = new HttpClient(new SslContextFactory());
+            try {
+                httpClient.start();
+                Request req = httpClient.newRequest(url)
+                        .method(HttpMethod.GET)
+                        .agent("java client")
+                        .header(HttpHeader.CONTENT_TYPE, "application/json");
+                ContentResponse resp = req.send();
+
+                String contentAsString = resp.getContentAsString();
+                ObjectMapper mapper = new ObjectMapper();
+                People character = mapper.readValue(contentAsString, People.class);
+                httpClient.stop();
+                context.setVariable("character", character);
+                engine.process("character.html", context, response.getWriter());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+        }
+
+    }
+
+}
