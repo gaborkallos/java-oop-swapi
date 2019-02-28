9bc6f37c6f22ec694f64f9b9d8181208a748e092
gaborKriger <regirck@gmail.com>
2019-02-28 10:28:17 +0100

    hide navbar if not on main screen PlanetsController

--- src/main/java/com/codecool/swapi/controller/PlanetsController.java
+++ src/main/java/com/codecool/swapi/controller/PlanetsController.java
@@ -1,7 +1,9 @@
 package com.codecool.swapi.controller;
 
 import com.codecool.swapi.config.TemplateEngineUtil;
+import com.codecool.swapi.models.Planet;
 import com.codecool.swapi.models.PlanetPage;
+import com.codecool.swapi.models.Vehicle;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.eclipse.jetty.client.HttpClient;
@@ -19,6 +21,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 
@@ -61,19 +66,58 @@ public class PlanetsController extends HttpServlet {
         WebContext context = new WebContext(request, response, request.getServletContext());
         context.setVariable("pagenumber", pageNumber);
         context.setVariable("planets", Objects.requireNonNull(getPlanetData()).getResults());
+        context.setVariable("navbar", true);
         engine.process("index.html", context, response.getWriter());
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if (request.getParameter("next") != null) {
-            pageNumber++;
-        } else if (request.getParameter("previous") != null) {
-            pageNumber--;
+        if (request.getParameter("next") != null || request.getParameter("previous") != null) {
+            if (request.getParameter("next") != null) {
+                pageNumber++;
+            } else if (request.getParameter("previous") != null) {
+                pageNumber--;
+            } else {
+                // TODO error handling
+            }
+            response.sendRedirect("/");
+        } else if (request.getParameter("planets") != null) {
+            TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
+            WebContext context = new WebContext(request, response, request.getServletContext());
+            response.setCharacterEncoding("utf-8");
+            String planetsURL = request.getParameter("planets");
+            if (planetsURL != null) {
+                List<String> planetsUrlList = Arrays.asList(planetsURL.substring(1, planetsURL.length() - 1).split(", "));
+                List<Planet> planets = new ArrayList<>();
+                for (String url : planetsUrlList) {
+                    HttpClient httpClient = new HttpClient(new SslContextFactory());
+
+                    try {
+                        httpClient.start();
+                        Request req = httpClient.newRequest(url)
+                                .method(HttpMethod.GET)
+                                .agent("java client")
+                                .header(HttpHeader.CONTENT_TYPE, "application/json");
+                        ContentResponse resp = req.send();
+
+                        String contentAsString = resp.getContentAsString();
+                        ObjectMapper mapper = new ObjectMapper();
+                        Planet planet= mapper.readValue(contentAsString, Planet.class);
+                        planets.add(planet);
+                        httpClient.stop();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+                context.setVariable("planets", planets);
+                context.setVariable("navbar", false);
+                engine.process("index.html", context, response.getWriter());
+            } else {
+                response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            }
         } else {
             // TODO error handling
         }
-        response.sendRedirect("/");
     }
 }
 
